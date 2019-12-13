c78dc3b8496cd35864051c506d51c63d523dbf4c
Rob Lyon <sir.rob.lyon@gmail.com>
2019-12-13 20:09:48 +0000

    Updates

---
+++ precache-manifest.9276829287e5b2026eb8213e39da3cd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "201cbcd1162f3295495530659742c207",
+    "url": "/Monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "38d3463cf3cadcc39e75",
+    "url": "/Monsters-Rolodex/static/css/main.e456e10d.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/Monsters-Rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Monsters-Rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "38d3463cf3cadcc39e75",
+    "url": "/Monsters-Rolodex/static/js/main.45285951.chunk.js"
+  },
+  {
+    "revision": "29f656592ba48eaf223e",
+    "url": "/Monsters-Rolodex/static/js/runtime-main.166da6c4.js"
+  }
+]);
